22a699ad61aaae08ded67f0c92abd27d751a6fdb
Luis M Canaval S <public02@canaval.org>
2023-10-31 14:46:12 -0500

    greedy coisn

---
+++ 1201_greedy_coins.py
@@ -0,0 +1,17 @@
+def coins(denominations: list, amount: int) -> dict:
+    detail = {}
+    for di in denominations:
+        if di <= amount:
+            detail[di] = amount // di
+            amount = amount % di
+
+    return detail
+
+
+denominations = [50, 20, 10, 5, 1]
+print("40:", coins(denominations, 40))
+print("87:", coins(denominations, 87))
+
+denominations = [50, 25, 20, 10, 5, 1]
+print("40:", coins(denominations, 40))
+print("87:", coins(denominations, 87))
