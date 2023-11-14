789979fd79553cc4942d14c04458aff8f6df8029
Luis M Canaval S <public02@canaval.org>
2023-11-14 11:59:36 -0500

    johnson

---
+++ 1401_johnson.py
@@ -0,0 +1,42 @@
+from utils import graphs
+
+def johnson(G):
+    n = len(G)
+    G.append([(n-1, 0)])
+    _, h = graphs.bellmanFord(G, n)
+    if not h:
+        return None, None # negative cycle
+    G.pop()
+    G2 = [[] for _ in range(n)]
+    for u in range(n):
+        for v, w in G[u]:
+            G2[u].append((v, w + h[u] - h[v]))
+
+    paths = []
+    costs = []
+    for u in range(n):
+        path, cost = graphs.dijkstra(G2, u)
+        paths.append(path)
+        costs.append(cost)
+
+    return paths, costs
+
+def solve():
+    f = open('2.graph')
+    labels = f.readline().split()
+    lbl2idx = {lbl: idx for idx, lbl in enumerate(labels)}
+    n = len(labels)
+    G = [[] for _ in range(n)]
+    for line in f:
+        u, v, w = [int(v) if i == 2 else lbl2idx[v]
+            for i, v in enumerate(line.split())]
+        G[u].append((v, w))
+
+    paths, costs = johnson(G)
+    if paths:
+        print(paths)
+        print(costs)
+        graphs.show(G, labels=labels, directed=True, weighted=True,
+                    path=paths[2]).view()
+
+solve()
--- utils/graphs.py
+++ utils/graphs.py
@@ -1,6 +1,8 @@
 import graphviz as gv
+import math
+import heapq
 
-def show(G, directed=False, weighted=False, path=[], layout="sfdp"):
+def show(G, labels=[], directed=False, weighted=False, path=[], layout="sfdp"):
   graph = gv.Digraph("felicidad") if directed else gv.Graph("alegria")
   graph.format = 'svg'
   graph.graph_attr["layout"] = layout
@@ -14,6 +16,9 @@ def show(G, directed=False, weighted=False, path=[], layout="sfdp"):
   graph.edge_attr["fontsize"] = "8"
   graph.edge_attr["fontname"] = "monospace"
   n = len(G)
+  if labels:
+        for i, lbl in enumerate(labels):
+            graph.node(str(i), lbl)
   added = set()
   for v, u in enumerate(path):
     if u != -1:
@@ -48,3 +53,45 @@ def show(G, directed=False, weighted=False, path=[], layout="sfdp"):
         else:
           graph.edge(str(u), str(v))
   return graph
+
+def bellmanFord(G, s):
+    n = len(G)
+    g = [math.inf]*n
+    path = [-1]*n
+    g[s] = 0
+
+    for _ in range(n-1):
+        for u in range(n):
+            for v, w in G[u]:
+                if g[u] + w < g[v]:
+                    g[v] = g[u] + w
+                    path[v] = u
+
+    for u in range(n):
+        for v, w in G[u]:
+            if g[u] + w < g[v]:
+                return None, None
+
+    return path, g
+
+def dijkstra(G, s):
+    n = len(G)
+    visited = [False]*n
+    path = [-1]*n
+    g = [math.inf]*n
+    g[s] = 0
+    pq = []
+    heapq.heappush(pq, (0, s))
+    while pq:
+        _, u = heapq.heappop(pq)
+        if visited[u]:
+            continue
+        visited[u] = True
+        for v, w in G[u]:
+            if g[u] + w < g[v]:
+                g[v] = g[u] + w
+                path[v] = u
+                heapq.heappush(pq, (g[v], v))
+
+    return path, g
+
